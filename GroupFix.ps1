44552015198224345e1da844933a5667b566dceb
Andrii Dykhlin <andrii@dykhl.in>
2022-01-01 19:58:09 +0100

    Typo

--- GroupFix.ps1
+++ GroupFix.ps1
@@ -1,7 +1,7 @@
 <#
 This script is for removing a user from a group B if they are members of group A. You can change the names of course to fulfill your needs. 
 Don't forget to change on line 6,7 and 12. Identities "Group 1" and "Group 2" are used for the generalization 
-And it will be more convenient to user samaccountname as a property.
+And it will be more convenient to use samaccountname as a property.
 
 Related post: https://dykhl.in/post/7
 #>
