5c725362ca243850a2bc576c35c1caa549882396
Andrii Dykhlin <andrii@dykhl.in>
2022-01-01 14:33:39 +0100

    typo fix

--- PasswordExpiry.ps1
+++ PasswordExpiry.ps1
@@ -1,6 +1,6 @@
 <#
 We check for the password expiry date for the accounts. Function in the end could be used in PowerShell $Profile to make the check easier. You can change the function name to something else if you want.
-Realted post: https://dykhl.in/post/8
+Related post: https://dykhl.in/post/8
 #>
 
 $user = Read-Host -Prompt "Username or part of it"
